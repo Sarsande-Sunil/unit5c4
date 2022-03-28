4c0eb393be4c3689109ebe6f74fde131296abf4c
Sarsande-Sunil <93374701+Sarsande-Sunil@users.noreply.github.com>
2022-03-28 16:25:22 +0530

    7

--- unit5-c4/src/Components/Navbar/Navbar.jsx
+++ unit5-c4/src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,22 @@
 import { Link } from "react-router-dom";
-
+import { Style } from "../../styling/navbarstyle";
 export const Navbar = () => {
   return (
-    <div className="navbar">
-      <Link className="navbarLoginSignUp" to={"/loginsignup"}>
-        Login/Sign Up
-      </Link>
-      <Link className="navbarLoginSignUp" to={"/meetup"}>
-        meetup
-      </Link>
-      <Link className="navbarLoginSignUp" to={"/event"}>
-        event
-      </Link>
-      <Link className="navbarLoginSignUp" to={"/404"}>
-        NotFound
-      </Link>
-    </div>
+    <Style>
+      <div className="navbar">
+        <Link className="navbarLoginSignUp" to={"/loginsignup"}>
+          Login/Sign Up
+        </Link>
+        <Link className="navbarLoginSignUp" to={"/meetup"}>
+          meetup
+        </Link>
+        <Link className="navbarLoginSignUp" to={"/event"}>
+          event
+        </Link>
+        <Link className="navbarLoginSignUp" to={"/404"}>
+          NotFound
+        </Link>
+      </div>
+    </Style>
   );
 };
---
+++ unit5-c4/src/styling/navbarstyle.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+const Style = styled.div`
+    font-size: 20px;
+    color: #fff;
+    display: flex;
+    justify-content: space-between;
+    margin-left: 400px;
+    &:h1{
+        color: black;
+    }
+    &:p{
+        color: black;
+    }
+`;
+export{Style}
